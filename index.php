b6795c265b30cee807fb2d94898a5bb5bb1402f4
Joe Sweeney <joe.sweeney224@gmail.com>
2015-08-27 00:03:12 -0600

    Cleanup

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
+// Get the file and put its contents in a string
 $input_file = fopen("input.txt","r") or die("Unable to open file");
 $input_string = fread($input_file, filesize("input.txt"));
 
-// Recursive function for getting marker positions
+// Recursive function for getting marker positions. Found this nice gem by martijn. tweaked slightly:
+// http://www.php.net/manual/en/function.strpos.php#107678
 function strpos_recursive($haystack, $needle, $offset = 0, &$results = []) {
     $offset = strpos($haystack, $needle, $offset);
     if($offset === false) {
@@ -17,36 +19,26 @@ function strpos_recursive($haystack, $needle, $offset = 0, &$results = []) {
 // Get marker positions
 $markers = strpos_recursive($input_string, "#");
 
-echo "Markers: ";
-foreach($markers as $i => $marker) {
-    echo $marker;
-    if ($i != count($markers) - 1) {
-        echo ",";
-    }
-}
-echo "<br>";
-
 // Iterate through the array of markers, and save the last path string before running the next loop
 for ($k = 0; $k < count($markers) - 1; $k++) {
-    // First marker
+
+    // Initialize variables
     $first_marker = $markers[$k];
-    echo "First marker: " . $first_marker . "<br>";
     $second_marker = $markers[$k + 1];
-    echo "second marker position: " . $second_marker . "<br>";
-    $newline_interval = strpos($input_string, "\n");
-    echo "first newline position: " . $newline_interval . "<br>";
+    $newline_interval = strpos($input_string, "\n"); // Frequency of newlines, mainly to determine vertical path
+    $marker_newline_difference = ($first_marker % $newline_interval); // What "distance from the left" is the first marker?
 
-    $marker_newline_difference = ($first_marker % $newline_interval);
-    echo "start/newline remainder: " . $marker_newline_difference . "<br>";
-
-
+    // If it's the first loop iteration, save the initial input string to the main string variable
     if ($k == 0) {
         $last_string = $input_string;
     }
-    
+
+    // Make the first marker the last known asterisk. Needed for horizontal markers to work
     $last_asterisk_position = $first_marker;
 
-// Should I go vertical?
+    // Should I go vertical? Check to see if the second marker is on the same line
+    // If not, write an asterisk at each newline interval
+    // Finish when on the line before the second marker
     if ($second_marker > $first_marker - $marker_newline_difference) {
         for ($i = $first_marker; $i < $second_marker; $i++) {
             if ($i % ($newline_interval + 1) == ($marker_newline_difference - 2) && $i != $first_marker) {
@@ -57,17 +49,14 @@ for ($k = 0; $k < count($markers) - 1; $k++) {
         }
     }
 
-    echo "downward done!";
-
-// If second marker is left of first marker...
+    // If second marker is left of first marker...
     if ($second_marker > $last_asterisk_position + ($newline_interval - $marker_newline_difference)) {
         for ($i = $second_marker + 1; $i < $last_asterisk_position + $newline_interval + 2; $i++) {
             $output_string = substr_replace($last_string, "*", $i, 1);
             $last_string = $output_string;
         }
     } else {
-
-// If right of first marker...
+        // If right of first marker...
         for ($i = $last_asterisk_position + 1; $i < $second_marker; $i++) {
             if ($i < $second_marker) {
                 $output_string = substr_replace($last_string, "*", $i, 1);
@@ -77,8 +66,8 @@ for ($k = 0; $k < count($markers) - 1; $k++) {
     }
 }
 
+// Save string to file and close
 $output_file = fopen("output.txt", "w");
 fwrite($output_file, $output_string);
 fclose($output_file);
-
 fclose($input_file);
