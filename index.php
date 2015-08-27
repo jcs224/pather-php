0ac435b3bd0e83f96a095be8ddbaf94943ff34ce
Joe Sweeney <joe.sweeney224@gmail.com>
2015-08-26 23:11:55 -0600

    Can run all the tests

--- index.php
+++ index.php
@@ -1,72 +1,78 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joe
- * Date: 8/25/15
- * Time: 7:23 PM
- */
-
-//$myfile = fopen("testfile.txt", "w");
-//$text = "........................\n........................";
-//fwrite($myfile, $text);
-//fclose($myfile);
 
 $input_file = fopen("input.txt","r") or die("Unable to open file");
 $input_string = fread($input_file, filesize("input.txt"));
 
-echo $input_string . "<br>";
-
-$num_markers = substr_count($input_string, "#");
-echo "number of markers: " . $num_markers . "<br>";
-
-$start_position = strpos($input_string, "#");
-echo "start position: " . $start_position . "<br>";
-
-$start_newline = strpos($input_string, "\n");
-echo "first newline position: " . $start_newline . "<br>";
-
-$position_newline_difference = ($start_position % $start_newline);
-echo "start/newline remainder: " . $position_newline_difference . "<br>";
-
-$second_position = strpos($input_string, "#", strpos($input_string, "#") + 1);
-echo "second marker position: " . $second_position . "<br>";
-
-$second_position_newline_difference = ($second_position % $start_newline);
-echo "second start/newline remainder: " . $second_position_newline_difference . "<br>";
-
-$last_string = $input_string;
-$last_asterisk_position = $start_position;
-
-// Should I go vertical?
-if ($second_position > $start_position - $position_newline_difference) {
-    for ($i = $start_position; $i < $second_position; $i++) {
-        if ($i % ($start_newline + 1) == ($position_newline_difference - 2) && $i != $start_position) {
-            $output_string = substr_replace($last_string, "*", $i, 1);
-            $last_string = $output_string;
-            $last_asterisk_position = $i;
-            echo "Last asterisk position: " . $last_asterisk_position;
-            echo $output_string . "<br>";
-        }
+// Recursive function for getting marker positions
+function strpos_recursive($haystack, $needle, $offset = 0, &$results = []) {
+    $offset = strpos($haystack, $needle, $offset);
+    if($offset === false) {
+        return $results;
+    } else {
+        $results[] = $offset;
+        return strpos_recursive($haystack, $needle, $offset + 1, $results);
     }
 }
 
-echo "downward done!";
+// Get marker positions
+$markers = strpos_recursive($input_string, "#");
+
+echo "Markers: ";
+foreach($markers as $i => $marker) {
+    echo $marker;
+    if ($i != count($markers) - 1) {
+        echo ",";
+    }
+}
+echo "<br>";
+
+// Iterate through the array of markers, and save the last path string before running the next loop
+for ($k = 0; $k < count($markers) - 1; $k++) {
+    // First marker
+    $first_marker = $markers[$k];
+    echo "First marker: " . $first_marker . "<br>";
+    $second_marker = $markers[$k + 1];
+    echo "second marker position: " . $second_marker . "<br>";
+    $newline_interval = strpos($input_string, "\n");
+    echo "first newline position: " . $newline_interval . "<br>";
+
+    $marker_newline_difference = ($first_marker % $newline_interval);
+    echo "start/newline remainder: " . $marker_newline_difference . "<br>";
+
+
+    if ($k == 0) {
+        $last_string = $input_string;
+    }
+    
+    $last_asterisk_position = $first_marker;
+
+// Should I go vertical?
+    if ($second_marker > $first_marker - $marker_newline_difference) {
+        for ($i = $first_marker; $i < $second_marker; $i++) {
+            if ($i % ($newline_interval + 1) == ($marker_newline_difference - 2) && $i != $first_marker) {
+                $output_string = substr_replace($last_string, "*", $i, 1);
+                $last_string = $output_string;
+                $last_asterisk_position = $i;
+            }
+        }
+    }
+
+    echo "downward done!";
 
 // If second marker is left of first marker...
-if ($second_position > $last_asterisk_position + ($start_newline - $position_newline_difference)) {
-    for ($i = $second_position + 1; $i < $last_asterisk_position + $start_newline + 2; $i++) {
-        $output_string = substr_replace($last_string, "*", $i, 1);
-        $last_string = $output_string;
-        echo $output_string . "<br>";
-    }
-} else {
-
-// If right of first marker...
-    for ($i = $last_asterisk_position + 1; $i < $second_position; $i++) {
-        if ($i < $second_position) {
+    if ($second_marker > $last_asterisk_position + ($newline_interval - $marker_newline_difference)) {
+        for ($i = $second_marker + 1; $i < $last_asterisk_position + $newline_interval + 2; $i++) {
             $output_string = substr_replace($last_string, "*", $i, 1);
             $last_string = $output_string;
-            echo $output_string . "<br>";
+        }
+    } else {
+
+// If right of first marker...
+        for ($i = $last_asterisk_position + 1; $i < $second_marker; $i++) {
+            if ($i < $second_marker) {
+                $output_string = substr_replace($last_string, "*", $i, 1);
+                $last_string = $output_string;
+            }
         }
     }
 }
